4db857357a2b985cf70bb9f8b94d80967761a9ea
pavankumar <pyangaldas@deloitte.com>
2018-06-04 01:36:37 +0530

    test case order

---
+++ src/test/java/junit/junitSamples/TestCaseOrder.java
@@ -0,0 +1,27 @@
+package junit.junitSamples;
+
+import static org.junit.Assert.*;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestCaseOrder {
+
+		@Test
+	    public void secondTest() {
+	        System.out.println("Executing second test");
+	    }
+	 
+	    @Test
+	    public void firstTest() {
+	        System.out.println("Executing first test");
+	    }
+	 
+	    @Test
+	    public void thirdTest() {
+	        System.out.println("Executing third test");
+	    }
+
+}
